6c0b2dd95f6602925eedbabc5ec21b10adc4b552
DrMaddMam <Luth.passhacker@gmail.com>
2025-09-05 17:36:30 -0400

    dscfsdf

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,7 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
     <>
-    <h1>Hello</h1>
-    <div className="alert alert-primary" role="alert">
-      A simple primary alert—check it out!
-    </div>
-</>
+    <div></div>
+    </>
   );
 }
